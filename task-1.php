4adbafbf4a635010f8605c4ce99470bdaee676d6
Selim Reza <selim16316@gmail.com>
2023-09-20 19:02:51 +0600

    Update task-1.php

--- task-1.php
+++ task-1.php
@@ -1,10 +1,37 @@
 <?php
-function printEvenNumbers($start, $end, $step) {
+function printEvenNumbersFor($start, $end, $step) {
     for ($i = $start; $i <= $end; $i += $step) {
         echo $i . " "; 
     }
 }
 
+echo "Even numbers for loop:";
+printEvenNumbersFor(2, 20, 2);
 
-printEvenNumbers(2, 20, 2);
-?>
+echo"<br/>";
+
+function printEvenNumbersWhile($start, $end, $step) {
+    while ($start <= $end) {
+        echo $start . " "; 
+        $start += $step;   
+    }
+}
+
+echo "Even numbers while loop:";
+printEvenNumbersWhile(2, 20, 2);
+
+echo"<br/>";
+
+
+function printEvenNumbersDoWhile($start, $end, $step) {
+    do {
+        echo $start . " "; 
+        $start += $step;   
+    } while ($start <= $end);
+}
+echo "Even numbers do while loop:";
+printEvenNumbersDoWhile(2, 20, 2);
+
+
+
+
